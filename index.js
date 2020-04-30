d6e703ab898feda7d0d4d923c410a29eddbab3fa
Elliott Shugerman <eeshugerman@gmail.com>
2020-04-30 15:42:18 -0600

    "open source contributions" section

--- index.js
+++ index.js
@@ -29,6 +29,17 @@ function render(resume) {
   });
 }
 
+// doesn't work :(
+const pdfRenderOptions = {
+  margin: {
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
+  },
+};
+
 module.exports = {
   render,
+  pdfRenderOptions,
 };
